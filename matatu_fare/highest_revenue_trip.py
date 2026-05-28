a1940391c54c33aa21bfd0a526fd934bbe738fad
Kimaiyoo <naomijepkorir18@gmail.com>
2026-05-28 13:04:13 +0300

    Add highest_revenue_trip - closes #87

---
+++ matatu_fare/highest_revenue_trip.py
@@ -0,0 +1,13 @@
+def get_best_trip(trips):  
+    # initialize first trip as best
+    best_trip = trips[0] 
+
+    # iterate through list to get the highest revenue trip
+    for trip in trips:
+        if trip['fare_paid'] > best_trip['fare_paid']:
+            best_trip = trip
+    return best_trip
+    
+# test
+trips = [{'route_name':'A','passengers':10,'fare_paid':500},{'route_name':'B','passengers':8,'fare_paid':1000}]
+print(get_best_trip(trips)['route_name'])

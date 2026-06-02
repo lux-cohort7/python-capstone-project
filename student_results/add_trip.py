ffc609a61d21a87d4cce8c6c3f39fe0108a59677
akellomichael <michaelomondiakello@gmail.com>
2026-06-02 09:56:51 +0300

    Add add_trip - closses #81

---
+++ student_results/add_trip.py
@@ -0,0 +1,30 @@
+def add_trip(trips, route_name, passengers, fare_paid):
+    """
+    Creates a trip dict and appends to trips list
+
+    Parameters
+    ----------
+    route_name : stringSS
+        name of route done
+    passengers : int
+        number of passangers on board.
+    fare_paid : float
+        sum of fare collected.
+
+    Returns
+    -------
+    list
+        updated list(trips) 
+    """
+    trip = {
+        "route_name": route_name,
+        "passengers": passengers,
+        "fare_paid": fare_paid
+        }
+    trips.append(trip)
+    return trips
+
+
+
+trips = []
+print(add_trip(trips,'CBD to Karen',10,1000))
